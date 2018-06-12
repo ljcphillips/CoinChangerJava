8e2404707dec6604d561744c2787c6b2ca10e17a
Lorraine Philllips <ljcphillips@gmail.com>
2018-06-12 21:22:51 +0100

    all tests passing

--- src/main/java/CoinChanger.java
+++ src/main/java/CoinChanger.java
@@ -4,7 +4,15 @@ import java.util.List;
 public class CoinChanger {
   public static List<Integer> giveChange(int amount){
     List<Integer> change = new ArrayList<>();
-    change.add(1);
+
+    int[]COINS = {50,20,10,5,2,1};
+
+    for (int i = 0; i < COINS.length; i++){
+      while(amount >= COINS[i]){
+        change.add(COINS[i]);
+        amount -= COINS[i];
+      }
+    }
     return change;
   }
 
--- src/test/java/CoinChangerTest.java
+++ src/test/java/CoinChangerTest.java
@@ -16,4 +16,45 @@ public class CoinChangerTest {
   public void changeFor1(){
       assertEquals(coins(1), CoinChanger.giveChange(1));
   }
+
+  @Test
+  public void changeFor2(){
+      assertEquals(coins(2), CoinChanger.giveChange(2));
+  }
+
+  @Test
+  public void changeFor3(){
+      assertEquals(coins(2,1), CoinChanger.giveChange(3));
+  }
+
+  @Test
+  public void changeFor5(){
+      assertEquals(coins(5), CoinChanger.giveChange(5));
+  }
+
+  @Test
+  public void changeFor7(){
+      assertEquals(coins(5,2), CoinChanger.giveChange(7));
+  }
+
+  @Test
+  public void changeFor10(){
+      assertEquals(coins(10), CoinChanger.giveChange(10));
+
+  }
+  @Test
+  public void changeFor20(){
+      assertEquals(coins(20), CoinChanger.giveChange(20));
+
+  }
+  @Test
+  public void changeFor50(){
+      assertEquals(coins(50), CoinChanger.giveChange(50));
+
+  }
+  @Test
+  public void changeFor99(){
+      assertEquals(coins(50,20,20,5,2,2), CoinChanger.giveChange(99));
+
+  }
 }
